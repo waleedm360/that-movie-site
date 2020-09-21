8588bbb195f1a8db364c541acf9b74a608ab6561
waleedm360 <63314875+waleedm360@users.noreply.github.com>
2020-09-21 12:02:09 -0700

    Update models.py

--- movie_site_app/models.py
+++ movie_site_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now=True)
+  updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Retailer(models.Model):
+	name = models.CharField(max_length=100)
+	movies = models.ManyToManyField(Movie, related_name='retailer')
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
+
+class Price(models.Model):
+	name = models.CharField(max_length=100)
+	movies = models.ManyToManyField(Movie, related_name='price')
+	created_at = models.DateTimeField(auto_now=True)
+	updated_at = models.DateTimeField(auto_now_add=True)
